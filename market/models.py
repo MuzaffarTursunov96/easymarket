90f100db59cbe0a4f0f35cc74e2f7c1a31fb11f3
MuzaffarTursunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-05-10 18:26:26 +0500

    new changes

--- market/models.py
+++ market/models.py
@@ -5,7 +5,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 class Categories(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-
+    
     class MPTTMeta:
         order_insertion_by = ['name']
     
