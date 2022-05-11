b954cb8bba2eca0387cb482a3157c109b42f8b64
MuzaffarTursunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-05-11 18:09:00 +0500

    ctegory changed

--- market/categories.py
+++ market/categories.py
@@ -1,5 +1,5 @@
 from market.models import Categories
 
 def categories_all(request):
-    categories = Categories.objects.all()
+    categories = Categories.objects.filter(parent=None)
     return {'categories':categories}
